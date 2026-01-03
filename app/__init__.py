286f081d5ed289a00c6eff2cf3a798daeab324bc
default <pryce@pryce.fyi>
2026-01-03 18:41:50 +0000

    Cleaned up pre-refactor assets

--- app/__init__.py
+++
@@ -1,3 +0,0 @@
-"""Helf - Health & Fitness Tracker."""
-
-__version__ = "0.1.0"
--- run.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env python3
-"""Entrypoint for Helf application."""
-
-from app import workout_tracker  # noqa: F401
-# Importing workout_tracker automatically starts the app via ui.run()
